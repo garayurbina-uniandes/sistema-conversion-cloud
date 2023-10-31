20b956dfe539c5548c7c3ad89d0172d4897a9cf5
garayurbina-uniandes <j.garay@uniandes.edu.co>
2023-10-30 19:38:47 -0500

    Update email.py for default quality gate

--- flaskr/utils/email.py
+++ flaskr/utils/email.py
@@ -44,3 +44,23 @@ def emailSecond(tarea: Tarea,usuario: Usuario):
     connection.login(username,password)
     connection.send_message(mimemsg)
     connection.quit()
+
+def email_third(tarea: Tarea,usuario: Usuario):
+
+    username = "recursosimp@gmail.com"
+    password = "pepesitos"
+    mail_from = "recursosimp@gmail.com"
+    mail_to = usuario.email
+    mail_subject = "Conversión lista"
+    mail_body = "Estimado " + usuario.username + " su archivo convertido está listo para ser descargado"
+
+    mimemsg = MIMEMultipart()
+    mimemsg['From']=mail_from
+    mimemsg['To']=mail_to
+    mimemsg['Subject']=mail_subject
+    mimemsg.attach(MIMEText(mail_body, 'plain'))
+    connection = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    connection.starttls()
+    connection.login(username,password)
+    connection.send_message(mimemsg)
+    connection.quit()
