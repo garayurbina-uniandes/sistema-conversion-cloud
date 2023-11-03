e8b08d72d8ba28545893d2ad3921175cdc7e4948
JAEL ALEXANDER RIVERA OVIEDO <ja.riverao1@uniandes.edu.co>
2023-11-03 12:39:56 -0500

    Make sure this PostgreSQL database password gets changed and removed from the code.
    https://sonarcloud.io/project/issues?open=AYtetHtY7xHaJhMF-pMA&id=garayurbina-uniandes_sistema-conversion-cloud

--- flaskr/config/default.py
+++ flaskr/config/default.py
@@ -1,1 +1,11 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@mi-database-01.ciksrkbct7fm.us-east-1.rds.amazonaws.com:5432/conversorDB'
+import os
+
+# Get the PostgreSQL password from an environment variable
+database_password = os.environ.get("DATABASE_PASSWORD")
+
+# Check if the password is available, otherwise, provide a default
+if not database_password:
+    database_password = "your_default_password"
+
+# Construct the SQLAlchemy Database URI
+SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:{database_password}@mi-database-01.ciksrkbct7fm.us-east-1.rds.amazonaws.com:5432/conversorDB"
